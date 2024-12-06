902e2d7212d05db9ed5132905c646b8a746992ad
mrkiffie <mrkiffie@gmail.com>
2024-12-07 01:14:43 +0200

    Update Day 06 - part 2 to use rayon

--- day-06/src/part2.rs
+++ day-06/src/part2.rs
@@ -1,4 +1,5 @@
 use grid::{Direction, Grid};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::{collections::BTreeSet, ops::Add};
 
 const INPUT: &str = include_str!("input.txt");
@@ -10,7 +11,7 @@ pub fn run() -> String {
 
 #[tracing::instrument(level = "trace", skip(input))]
 fn process(input: &str) -> u32 {
-    let get_direction = |i: usize| match i {
+    let get_direction = |i: u8| match i {
         x if x % 4 == 0 => Direction::N,
         x if x % 4 == 1 => Direction::E,
         x if x % 4 == 2 => Direction::S,
@@ -25,9 +26,9 @@ fn process(input: &str) -> u32 {
     let start = grid.index_to_vec2(start_index);
 
     let mut stepper = start.clone();
-    let mut stepper_direction: usize = 0;
+    let mut stepper_direction = 0;
 
-    let mut visited: BTreeSet<usize> = BTreeSet::new();
+    let mut visited: BTreeSet<u32> = BTreeSet::new();
 
     loop {
         // step
@@ -42,87 +43,85 @@ fn process(input: &str) -> u32 {
             stepper_direction &= 3;
         } else {
             // update map
-            visited.insert(grid.point_to_index(&next).expect("point should be valid"));
+            visited.insert(grid.point_to_index(&next).expect("point should be valid") as u32);
             stepper = next;
         }
     }
 
-    let mut obstacles = 0;
+    visited
+        .par_iter()
+        .map(|&obstacle_index| {
+            let obstacle = grid.index_to_vec2(obstacle_index as usize);
 
-    for &obstacle_index in visited.iter() {
-        let obstacle = grid.index_to_vec2(obstacle_index);
+            // Resume from previous fork or start at the beginning
+            let mut double_stepper = start.clone();
+            let mut single_stepper = start.clone();
+            // Direction index
+            let mut double_stepper_direction = 0;
+            let mut single_stepper_direction = 0;
 
-        // Resume from previous fork or start at the beginning
-        let mut double_stepper = start.clone();
-        let mut single_stepper = start.clone();
-        // Direction index
-        let mut double_stepper_direction = 0;
-        let mut single_stepper_direction = 0;
+            // loop detection makes use of 2 walkers following the path. The one
+            // walks twice as fast as the other. If they meet on the same spot and
+            // are facing the same direction, we've hit a loop. If the fast walker
+            // leaves the map, there is no loop.
+            loop {
+                // double stepper step 1
+                {
+                    let next = double_stepper.add(get_direction(double_stepper_direction));
+                    let Some(c) = grid.get(&next) else {
+                        return 0;
+                    };
 
-        // loop detection makes use of 2 walkers following the path. The one
-        // walks twice as fast as the other. If they meet on the same spot and
-        // are facing the same direction, we've hit a loop. If the fast walker
-        // leaves the map, there is no loop.
-        loop {
-            // double stepper step 1
-            {
-                let next = double_stepper.add(get_direction(double_stepper_direction));
-                let Some(c) = grid.get(&next) else {
-                    break;
-                };
+                    if c == '#' || obstacle == next {
+                        // turn
+                        double_stepper_direction += 1;
+                        double_stepper_direction &= 3;
+                    } else {
+                        double_stepper = next;
+                    }
+                }
 
-                if c == '#' || obstacle == next {
-                    // turn
-                    double_stepper_direction += 1;
-                    double_stepper_direction &= 3;
-                } else {
-                    double_stepper = next;
+                // double stepper step 2
+                {
+                    let next = double_stepper.add(get_direction(double_stepper_direction));
+                    let Some(c) = grid.get(&next) else {
+                        return 0;
+                    };
+
+                    if c == '#' || obstacle == next {
+                        // turn
+                        double_stepper_direction += 1;
+                        double_stepper_direction &= 3;
+                    } else {
+                        double_stepper = next;
+                    }
+                }
+
+                // single stepper
+                {
+                    let next = single_stepper.add(get_direction(single_stepper_direction));
+                    let Some(c) = grid.get(&next) else {
+                        return 0;
+                    };
+
+                    if c == '#' || obstacle == next {
+                        // turn
+                        single_stepper_direction += 1;
+                        single_stepper_direction &= 3;
+                    } else {
+                        single_stepper = next;
+                    }
+                }
+
+                // has double stepper caught up with single stepper?
+                if single_stepper == double_stepper
+                    && single_stepper_direction == double_stepper_direction
+                {
+                    return 1;
                 }
             }
-
-            // double stepper step 2
-            {
-                let next = double_stepper.add(get_direction(double_stepper_direction));
-                let Some(c) = grid.get(&next) else {
-                    break;
-                };
-
-                if c == '#' || obstacle == next {
-                    // turn
-                    double_stepper_direction += 1;
-                    double_stepper_direction &= 3;
-                } else {
-                    double_stepper = next;
-                }
-            }
-
-            // single stepper
-            {
-                let next = single_stepper.add(get_direction(single_stepper_direction));
-                let Some(c) = grid.get(&next) else {
-                    break;
-                };
-
-                if c == '#' || obstacle == next {
-                    // turn
-                    single_stepper_direction += 1;
-                    single_stepper_direction &= 3;
-                } else {
-                    single_stepper = next;
-                }
-            }
-
-            // has double stepper caught up with single stepper?
-            if single_stepper == double_stepper
-                && single_stepper_direction == double_stepper_direction
-            {
-                obstacles += 1;
-                break;
-            }
-        }
-    }
-
-    obstacles
+        })
+        .sum()
 }
 
 #[cfg(test)]
